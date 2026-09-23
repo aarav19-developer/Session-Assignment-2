1ebd2f6e78220bf811e6261b5389762f57d3ebfb
Aarav Harit <aaravharit418@gmail.com>
2026-09-23 21:41:04 +0530

    Session_Assignment

--- data/students.js
+++ data/students.js
@@ -1,7 +1,7 @@
 const students = [
   { id: 1, name: "Aarav Harit", age: 20, course: "B.Tech CSE" },
-  { id: 2, name: "Sakshi", age: 21, course: "BCA" },
-  { id: 3, name: "Teotia", age: 19, course: "B.Sc IT" }
+  { id: 2, name: "DEARS", age: 21, course: "BCA" },
+  { id: 3, name: "Zindagi", age: 19, course: "B.Sc IT" }
 ];
 
 module.exports = students;
